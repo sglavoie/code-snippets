6750cb7fc2ad84660bd588360b02383076edb874
Sébastien Lavoie <sgdlavoie@gmail.com>
2019-05-27 22:31:42 -0500

    Add missing docstrings for each function

--- python/dates/years.py
+++ python/dates/years.py
@@ -1,8 +1,10 @@
 def is_julian_leap(year: int) -> bool:
+    """Return whether a year is leap in a Gregorian calendar as a boolean."""
     return year % 4 == 0
 
 
 def is_gregorian_leap(year: int) -> bool:
+    """Return whether a year is leap in a Gregorian calendar as a boolean."""
     if year % 100 == 0:
         return year % 400 == 0
     return year % 4 == 0
